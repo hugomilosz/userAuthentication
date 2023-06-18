bd5a5f178820bc371b11a9294353c26ee36f4851
hugomiloszewski <hugomilosz@gmail.com>
2023-06-18 16:41:29 +0100

    Added accounts and core repos

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+from decouple import config
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(config("APP_SETTINGS"))
+
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from src.accounts.views import accounts_bp
+from src.core.views import core_bp
+
+app.register_blueprint(accounts_bp)
+app.register_blueprint(core_bp)
